592af1c5882526989911a65843d04e53a7169d50
catchKkkk <1790600763@qq.com>
2023-12-19 19:56:51 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<Addition />
+
+		<Subtraction />
+	</div>
+</template>
+
+<script>
+import Addition from "./components/Addition.vue";
+import Subtraction from "./components/Subtraction.vue";
+
+export default {
+	data() {
+		return {};
+	},
+	components: {
+		Addition,
+		Subtraction,
+	},
+};
+</script>
---
+++ src/components/Subtraction.vue
@@ -0,0 +1,12 @@
+<template>
+	<h3>当前最新的count值是：</h3>
+	<button>-1</button>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+};
+</script>
